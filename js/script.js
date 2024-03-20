8e76c7ede04480abaeab0f1773f97c8b0de9e6ee
ClaudioPerrone <cla-agadir97@hotmail.it>
2024-03-20 02:54:07 +0100

    Feat: added prompt timer

--- js/script.js
+++ js/script.js
@@ -10,16 +10,42 @@ Descrizione:
 
 
 //INPUT DATI
-//Creo un array con 5 numeri random da 1 a 100
-const numbers = [];
-console.log(numbers);
+//Evoco la funzione per generare un array di 5 numeri casuali compresi tra 1 e 100
+let arrayNumbers = randomNumbers(5, 1, 100);
+//console.log(arrayNumbers);
+
+//Timer per i 5 prompt
+setTimeout(promptNumbers, 4000);
+//console.log(promptNumbers());
 
 //ELABORAZIONE DATI
-//Creo 5 numeri random da 1 a 100
-for (let i = 0; i < 5; i++){
-    //Push i numeri random nell'array
-    let random = Math.floor(Math.random() * 100) + 1;
-    numbers.push(random)
-}
+
 
 //OUTPUT DATI
+//Ottengo il riferimento all'elemento HTML con id "array"
+let arrayElement = document.querySelector('#array');
+//Visualizzo l'array nella pagina convertendolo in una stringa
+arrayElement.textContent = arrayNumbers.join(', ');
+
+
+
+/*--------------------------*/
+//FUNCTIONS
+//Creo un array con 5 numeri random da 1 a 100
+function randomNumbers(length, min, max) {
+    let arrayNumbers = [];
+    for (let i = 0; i < length; i++) {
+        let random = Math.floor(Math.random() * (max - min + 1)) + min;
+        arrayNumbers.push(random);
+    }
+    return arrayNumbers;
+}
+//Creo 5 prompt per inserire i numeri
+function promptNumbers(){
+    let promptArray = [];
+    for (let i = 0; i < 2; i++){
+        let domanda = parseInt(prompt("Inserisci i numeri uno alla volta"));
+        promptArray.push(domanda);
+    }
+    return promptArray;
+}
