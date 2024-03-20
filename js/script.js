201c67efd8ff13ed9db2d592785322ccc506f4f8
ClaudioPerrone <cla-agadir97@hotmail.it>
2024-03-20 20:20:01 +0100

    Fix: changed function for random number

--- js/script.js
+++ js/script.js
@@ -8,44 +8,74 @@ Descrizione:
 - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
-
 //INPUT DATI
 //Evoco la funzione per generare un array di 5 numeri casuali compresi tra 1 e 100
-let arrayNumbers = randomNumbers(5, 1, 100);
+const arrayNumbers = generateRandomArray(5, 1, 10);
 //console.log(arrayNumbers);
 
-//Timer per i 5 prompt
-setTimeout(promptNumbers, 4000);
-//console.log(promptNumbers());
+//Timer per i 5 prompt di TOT secondi
+const timer = 5000;
+setTimeout(promptNumbers, timer);
+//console.log(promptArray);
 
 //ELABORAZIONE DATI
+if (setTimeout === 0){
+    if(randomNumbers === promptNumbers){
+        console.log("Hai indovinato");
+    } else{
+        console.log("Non hai indovinato");
+    }
+}
+
 
 
 //OUTPUT DATI
 //Ottengo il riferimento all'elemento HTML con id "array"
-let arrayElement = document.querySelector('#array');
+let arrayElement = document.querySelector('#arrayRandom');
 //Visualizzo l'array nella pagina convertendolo in una stringa
 arrayElement.textContent = arrayNumbers.join(', ');
 
-
-
 /*--------------------------*/
 //FUNCTIONS
-//Creo un array con 5 numeri random da 1 a 100
-function randomNumbers(length, min, max) {
-    let arrayNumbers = [];
-    for (let i = 0; i < length; i++) {
-        let random = Math.floor(Math.random() * (max - min + 1)) + min;
-        arrayNumbers.push(random);
+/*---------------------*/
+// Genera un array di arrayLength numeri random
+// arrayLength -> numero intero che definisce la lunghezza dell'array tornato
+// numMin -> numero intero minimo da generare
+// numMax -> numero intero massimo da generare
+// return: un array di arrayLength numeri random
+
+//const arrayNumbers = generateRandomArray(nx, ny, nz);
+
+function generateRandomArray(arrayLength, numMin, numMax) {
+    // Creiamo un array vuoto
+    const randomNumbersArray = [];
+
+    // finche non ci sono arrayLength numeri nell'array:
+    while(randomNumbersArray.length < arrayLength) {
+        // genero un numero random
+        const randNumber = getRndInteger(numMin, numMax);
+        // se il numero random non esiste nell'array lo pusho
+        if(!randomNumbersArray.includes(randNumber)) {
+            randomNumbersArray.push(randNumber);
+        }
     }
-    return arrayNumbers;
+    
+    return randomNumbersArray;
 }
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+/*---------------------*/
+/*---------------------*/
 //Creo 5 prompt per inserire i numeri
 function promptNumbers(){
     let promptArray = [];
-    for (let i = 0; i < 2; i++){
-        let domanda = parseInt(prompt("Inserisci i numeri uno alla volta"));
-        promptArray.push(domanda);
+    for (let i = 0; i < 5; i++){
+        let numberPrompt = parseInt(prompt("Inserisci i numeri uno alla volta"));
+        promptArray.push(numberPrompt);
     }
+    //console.log(promptArray);
     return promptArray;
 }
+/*---------------------*/
